4d095f77cc5a85860d36aa2f5909c7c5abcb25ea
eham757 <akash1999@hotmail.nl>
2017-11-17 08:38:30 +0100

    Add project files.

---
+++ Quizes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

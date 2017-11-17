6c7453cc2bd31887163baad42778660300e2c5be
eham757 <akash1999@hotmail.nl>
2017-11-17 11:54:43 +0100

    Revert "Added Questions to the Program"
    This reverts commit 69bec60e33cef3dd21f90645dbd31d3d36e1b2b6.

--- Quizes/Program.cs
+++ Quizes/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Quizes
 {
@@ -7,15 +6,7 @@ namespace Quizes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the quizmaster 9000 (name still pending)");
-            List<Question> questions = new List<Question>()
-            {
-                new Question(){Text = "What is 2+2 ?", Answer = "4", Difficulty = 1, Category = "Math" },
-                new Question(){Text = "What does LOIC stand for ?, write the awnser with spaces inbetween the words", Answer = "Low Orbital Ion Canon", Difficulty = 2, Category = "Hacking" },
-                new Question(){Text = "What is the technical term for the smallest piece of grammatical unit in a language", Answer = "Morpheme", Difficulty = 3, Category = "Language" }
-            };
-
-
+            Console.WriteLine("ChangedLine");
         }
     }
 }
--- Quizes/Question.cs
+++ Quizes/Question.cs
@@ -8,9 +8,6 @@ namespace Quizes
     {
         public string Text { get; set; }
         public string Answer { get; set; }
-        public int Difficulty { get; set; }
-        public string Category { get; set; }
-
 
         public bool checkAnswer(string response)
         {

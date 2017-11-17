69bec60e33cef3dd21f90645dbd31d3d36e1b2b6
eham757 <akash1999@hotmail.nl>
2017-11-17 09:44:04 +0100

    Added Questions to the Program

--- Quizes/Program.cs
+++ Quizes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Quizes
 {
@@ -6,7 +7,15 @@ namespace Quizes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ChangedLine");
+            Console.WriteLine("Welcome to the quizmaster 9000 (name still pending)");
+            List<Question> questions = new List<Question>()
+            {
+                new Question(){Text = "What is 2+2 ?", Answer = "4", Difficulty = 1, Category = "Math" },
+                new Question(){Text = "What does LOIC stand for ?, write the awnser with spaces inbetween the words", Answer = "Low Orbital Ion Canon", Difficulty = 2, Category = "Hacking" },
+                new Question(){Text = "What is the technical term for the smallest piece of grammatical unit in a language", Answer = "Morpheme", Difficulty = 3, Category = "Language" }
+            };
+
+
         }
     }
 }

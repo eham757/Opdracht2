66315b1fe3faaa9ed796672d109edf089f419591
eham757 <akash1999@hotmail.nl>
2017-11-17 09:27:17 +0100

    Question Class toegevoegd

---
+++ Quizes/Question.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quizes
+{
+    public class Question
+    {
+        public string Text { get; set; }
+        public string Answer { get; set; }
+
+        public bool checkAnswer(string response)
+        {
+            string responseLower = response.ToLower();
+            string answerLower = Answer.ToLower();
+            return responseLower == answerLower;
+        }
+
+        public void display()
+        {
+            Console.WriteLine(Text);
+        }
+    }
+}

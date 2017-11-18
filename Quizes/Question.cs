8db49c1de7d151d850da93f24b07d2e8a1dd7dc0
eham757 <akash1999@hotmail.nl>
2017-11-18 13:40:48 +0100

    small errs fixed

--- Quizes/Question.cs
+++ Quizes/Question.cs
@@ -8,6 +8,8 @@ namespace Quizes
     {
         public string Text { get; set; }
         public string Answer { get; set; }
+        public int Difficulty { get; set; }
+        public string Category { get; set; }
 
         public virtual bool checkAnswer(string response)
         {

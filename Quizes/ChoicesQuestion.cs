c2cc526e4781aa3d62fc2a7300794d35048105a4
eham757 <akash1999@hotmail.nl>
2017-11-18 10:44:56 +0100

    finished ChoicesQuistion class

--- Quizes/ChoicesQuestion.cs
+++ Quizes/ChoicesQuestion.cs
@@ -1,31 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace Quizes
 {
     public class ChoicesQuestion : Question
     {
-        private IList<string> choices = new List<string>();
-        
-        public void addChoice(string choice, bool isAnswer)
-        {
-            choices.Add(choice);
-            if (isAnswer)
-            {
-                Answer = choice;
-            }
-        }
+        // Text, Answer, Difficulty and Category are inherited 
+        //choices is private so a public Choices serves as the property 
 
-        public void display()
+        private IList<string> choices = new List<string>();
+
+        public IList<string> Choices { get => choices; set => choices = value; }
+
+        public override void display()
         {
             Console.WriteLine(Text);
             int count = 0;
             foreach(string s in choices)
             {
                 count++;
-                Console.WriteLine(string.Format("{0}. {1}"), count, s);
+                Console.WriteLine(String.Format("{0}: {1}", count, s));
             }
+        }
+
+        public override bool checkAnswer(string response)
+        {
+
+            int element = Int16.Parse(response)- 1;
+            return choices.ElementAt(element) == Answer;
         }
     }
 }
